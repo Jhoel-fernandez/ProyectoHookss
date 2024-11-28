3faed9f3100d305a31b6b12760f580d55cd1a8fb
Jhoel Fernandez <jhoelfernandez085@gmail.com>
2024-11-28 10:47:58 -0500

    Demostracion de hook formatp codigo

--- ProyectoHookss/Program.cs
+++ ProyectoHookss/Program.cs
@@ -1,2 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+Console.WriteLine("Prueba de formateo de codigo");
+int edad = 25;
+if (edad >= 18)
+{
+    Console.WriteLine("Es mayor de edad");
+}
+else
+{
+    Console.WriteLine("Es menor de edad");
+}
